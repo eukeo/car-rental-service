fc5c01408d9d9fa6124b6e57e0557901fdaa546b
eukeo <sixsblooms@gmail.com>
2024-10-15 12:36:03 -0700

    vehicles-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navigation from "./NavBar/NavBar";
 import HomeHeader from "./HomeHeader/HomeHeader";
+import Vehicles from "./Vehicles/Vehicles";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <HomeHeader />
+      <Vehicles />
     </div>
   );
 }
---
+++ src/Vehicles/Vehicles.js
@@ -0,0 +1,20 @@
+const Vehicles = () => {
+  return (
+    <div>
+      <div>
+        <h1>Luxury</h1>
+        <h4>Roll In Style</h4>
+      </div>
+      <div>
+        <h1>Suv</h1>
+        <h4>Family Friendly</h4>
+      </div>
+      <div>
+        <h1>Sedan</h1>
+        <h4>Casual Driver</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Vehicles;
