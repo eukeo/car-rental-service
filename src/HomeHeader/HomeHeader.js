8da43c3743e413c8f726a2855a3e936a4d4e6ac2
eukeo <sixsblooms@gmail.com>
2024-11-04 12:00:57 -0800

    nav-bar-css

--- src/HomeHeader/HomeHeader.js
+++ src/HomeHeader/HomeHeader.js
@@ -4,7 +4,7 @@ const HomeHeader = () => {
   return (
     <div className="banner">
       <div className="search">
-        <h5>Currently renting in Los Angeles, Chicago and New York.</h5>
+        <h5>Now renting in Los Angeles, Chicago and New York!</h5>
         <input className="input" placeholder="Search Your Location"></input>
         <button>Search</button>
       </div>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,13 +4,12 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 const Navigation = () => {
   return (
     <Navbar expand="lg">
-      <h1 className="header">Carma</h1>
-      <h5 className="quote">Good Rides, Good Carma.</h5>
       <Container className="navbar">
+        <Nav.Link href="#home" className="header">
+          Carma
+        </Nav.Link>
         <ul>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#locations">Locations</Nav.Link>
             <Nav.Link href="#vehicles">Vehicles</Nav.Link>
             <Nav.Link href="#reviews">Reviews</Nav.Link>
             <Nav.Link href="#faqs">FAQs</Nav.Link>
